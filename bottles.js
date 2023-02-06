71625bd441e17b5abaa0f3b8a8b104d4d216aa43
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-02-06 11:32:23 -0700

    99 bottles loop challenge

---
+++ bottles.js
@@ -0,0 +1,14 @@
+function bottles(num) {
+    let i = num;
+    while (i>=2){
+        console.log(`${i} bottles of beer on the wall, ${i} bottles of beer`);
+        console.log(`Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
+        i--;
+    }
+    console.log('1 bottle of beer on the wall, 1 bottle of beer.')
+    console.log('Take one down and pass it around, no more bottles of beer on the wall.')
+    console.log('No more bottles of beer on the wall, no more bottles of beer.')
+    console.log(`Go to the store and buy some more, ${num} bottles of beer on the wall.`)
+}
+
+bottles(99)
