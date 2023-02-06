57dfe589c7fefe94501aeab6afbd7ac67f5f287f
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-02-06 16:22:53 -0700

    practice with deaf grandmother still needs work

--- deaf.js
+++ deaf.js
@@ -1,18 +1,70 @@
 const prompt=require("prompt-sync")({sigint:true}); 
 
-function deafGrandma() {
-    let speech = prompt("HEY KID!");
+function canHear (speech) {
     let cantHear = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    if (speech == '') {
-         'WHAT?!';
+    if (speech.length <1) {
+        return false
     }
     speechArr = speech.split('')
-    for (let i=0; i<speechArr.length; i++) {
-        let char = speechArr[i];
-        if (char in cantHear) {
-            return 'SPEAK UP KID!'
-        }
-    }   
+    console.log(speechArr)
+    if (speechArr.length>=1) {
+        for (let i=0; i<speechArr.length; i++) {
+            let char = speechArr[i];
+            console.log(char)
+            for (let j=0; j<cantHear.length; j++) {
+                let letter = cantHear[j];
+                if (letter == char) {
+                    return false;
+                }
+            }}}
+            return true;
 }
 
-deafGrandma()
+
+function response(speech) {
+    let cantHear = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    if (speech.length <1) {
+        answer = prompt('WHAT?!\n');
+        response(answer)
+    }
+    speechArr = speech.split('')
+    console.log(speechArr)
+
+    if (speech.length>=1) {
+        for (let i=0; i<speechArr.length; i++) {
+            let char = speechArr[i];
+            console.log(char)
+            for (let j=0; j<cantHear.length; j++) {
+                let letter = cantHear[j];
+                if (char == letter) {
+                    answer = prompt('SPEAK UP KID!\n');
+                    response(answer);
+                } else {
+                    answer = prompt('NO, NOT SINCE 1946!\n')
+                    response(answer)
+                }
+            }
+        }  
+        response(answer)
+    }
+}
+
+function deafGrandma() {
+    let speech = prompt("HEY KID!\n");
+    let heard = canHear(speech);
+    if (heard == true) {
+        if (heard == 'GOODBYE!') {
+            let answer = prompt('LEAVING SO SOON? \n')
+            if (answer == 'GOODBYE!') {
+                return 'LATER, SKATER!'
+            }
+            response(speech)
+        }
+    }
+    if (heard == false) {
+        response(prompt('WHAT??\n'))
+    }
+}
+
+console.log(deafGrandma())
+//deafGrandma()
