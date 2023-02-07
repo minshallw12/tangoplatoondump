4359e69c29b944c8b796eff3f43ba56e0d4bd3d1
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-02-07 14:07:45 -0700

    factorial recursion practice

---
+++ factorial.js
@@ -0,0 +1,15 @@
+function factorial(num) {
+    if (num < 1) {
+        return 'bad input'
+    }
+    if (num == 1) {
+        return 1
+    }
+    return num * factorial(num-1)
+}
+
+console.log(factorial(4))
+console.log(factorial(7))
+console.log(factorial(8))
+console.log(factorial(1))
+console.log(factorial(0))
