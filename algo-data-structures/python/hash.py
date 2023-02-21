633fd1a027928165781e95a74f10769008d5afc4
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-02-21 15:52:23 -0700

    practice

--- algo-data-structures/python/hash.py
+++ algo-data-structures/python/hash.py
@@ -1,24 +1,40 @@
+import math
+
 class HashTable:
   def __init__(self, number_of_buckets):
-    # self.number_of_buckets = number_of_buckets
-    # self.table = [None] * self.number_of_buckets
+    self.number_of_buckets = number_of_buckets
+    self.table = [ [] for i in range(number_of_buckets)]
     pass
 
-  def hash(self, value):
-    # here is where you'll turn your 'value' into a hash value that will return the index of your table to insert value
-    # IMPORTANT: Think about how you'd store values into the same index
-    pass
+  def hash(self, key):
+    num = 0
+    for char in key:
+      num += ord(char)
+    m = 0.5*(math.sqrt(5)-1)
+    m = m + num
+    hashed = math.floor(m % self.number_of_buckets)
+    return hashed
 
-  def set(self, value):
+  def set_value(self, key, value):
     # here is where you'll perform your logic to insert the value into your table
     # you'll also call your hash method here to get the index where you'll store the value
-    pass
+    index = self.hash(key)
+    print(index)
+    self.table[index].append[(key, value)]
 
-  def get(self, value):
+
+  def get_value_by_key(self, key):
     # here is where you'll retreive your value from the hash table
     # IMPORTANT: Think about how you'd retreive a value that from an index that has multiple values
-    pass
+    index = self.hash(key)
+    for data in self.table[index]:
+      print(data)
 
   def has_key(self, value):
     # here is where you'll return a True or False value if there is a value stored at a specific index in your HashTable
     pass
+
+
+my_table = HashTable(64)
+my_table.set_value('name', 'alice')
+print(my_table.get_value_by_key("name"))
--- algo-data-structures/python/linked_list.py
+++ algo-data-structures/python/linked_list.py
@@ -1,25 +1,69 @@
-class LinkList:
-  # write your __init__ method here that should store a 'head' value which the first Node 
-  # in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
-  def __init__(self, head = None):
-    self.head = head
-    self.length = len(self)
-
-  def add(self, data):
-    # write your code to ADD an element to the Linked List
-    pass
-
-  def remove(self, data):
-    # write your code to REMOVE an element from the Linked List
-    pass
-
-  def get(self, element_to_get):
-    # write you code to GET and return an element from the Linked List
-    pass
-
 # ----- Node ------
 class Node:
   def __init__(self, value):
     self.value = value
-    self.next = None
+    self.nextNode = None
 
+
+class LinkList:
+  # write your __init__ method here that should store a 'head' value which the first Node 
+  # in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+  def __init__(self):
+    self.head = None
+    self.length = 0
+
+  def add(self, data):
+    if self.head == None:
+      self.head = Node(data)
+    else:
+      currentNode = self.head
+      while currentNode.nextNode != None:
+        currentNode = currentNode.nextNode
+      currentNode.nextNode = Node(data)
+      self.length += 1
+
+    # write your code to ADD an element to the Linked List
+    # self.length += 1
+    # if self.head == None:
+    #   self.head = Node(data)
+    #   return
+    # current = self.head
+    # while current.nextNode!= None:
+    #   current = current.nextNode
+    # current.nextNode = Node(data)
+    # current.nextNode.nextNode = None
+      
+
+  def remove(self, data):
+    if self.head == None:
+      return
+    if self.head.value == data:
+      self.head = self.head.nextNode
+      return
+    current = self.head
+    while current.nextNode != None:
+      if current.nextNode.value == data:
+        current.nextNode = current.nextNode.nextNode
+        return
+      current = current.nextNode
+      break   
+
+  def get(self, element_to_get):
+    currentNode = self.head
+    while currentNode.nextNode != None:
+      if element_to_get == currentNode.nextNode.value:
+        return currentNode.nextNode.value
+      currentNode = currentNode.nextNode
+        
+
+
+
+
+my_list = LinkList()
+my_list.add('will')
+my_list.add("oranges")
+my_list.add("toby")
+my_list.add("kfanifg")
+my_list.add(["Tiger Shark"])
+
+print(my_list.get("oranges"))
